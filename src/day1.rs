b950821e6dff661e8547c4844a4eef447ef36bdf
null-version-nova <ethcoyle@gmail.com>
2025-12-01 17:44:09 -0600

    finished day 1 :)

--- src/day1.rs
+++ src/day1.rs
@@ -1,1 +1,91 @@
-pub fn run() {}
+// Contains both day 1 answers!
+
+use std::str::FromStr;
+use std::{fs::File, io::Read};
+
+pub fn rotate(dial: i32, amount: i32) -> (i32, i32) {
+    let mut result = dial + amount;
+    let mut zeroes = 0;
+    let mut start_at_zero = dial == 0;
+    while result < 0 {
+        result += 100;
+        if start_at_zero {
+            start_at_zero = false; // eek!
+        } else {
+            zeroes += 1;
+        }
+    }
+    while result > 99 {
+        result -= 100;
+        zeroes += 1;
+    }
+    if amount < 0 && result == 0 {
+        zeroes += 1;
+    }
+    (result, zeroes)
+}
+
+pub fn read_from_file(path: &str) -> String {
+    let mut file = File::open(path).expect("input load failed :(");
+    let mut text = String::new();
+    file.read_to_string(&mut text)
+        .expect("Reading from string failed :(");
+    text
+}
+
+pub fn read_to_iterator(text: &str) -> impl Iterator<Item = i32> {
+    text.split('\n').filter_map(|line| {
+        let mut result = 1;
+        if line.contains('R') {
+            result *= 1;
+        } else if line.contains('L') {
+            result *= -1;
+        } else {
+            return None;
+        }
+        let number = line.get(1..)?;
+        result *= i32::from_str(number).ok()?;
+        // println!("{} read as {}", line, result);
+        Some(result)
+    })
+}
+
+pub fn part1() {
+    let file = read_from_file("./inputs/1.txt");
+    let lines = read_to_iterator(file.as_str());
+
+    let mut dial = 50;
+    let mut zeroes = 0;
+    for line in lines {
+        dial = rotate(dial, line).0;
+        if dial == 0 {
+            zeroes += 1;
+        }
+        println!(
+            "Dial rotated by {} to {}, {} zeroes found...",
+            line, dial, zeroes
+        );
+    }
+    println!("Dial reads {}!", dial);
+    println!("{} zeroes found!", zeroes);
+}
+
+pub fn part2() {
+    let file = read_from_file("./inputs/1.txt");
+    let lines = read_to_iterator(file.as_str());
+
+    let mut dial = 50;
+    let mut zeroes = 0;
+    let mut rotation: (i32, i32);
+    for line in lines {
+        rotation = rotate(dial, line);
+        dial = rotation.0;
+        zeroes += rotation.1;
+        println!(
+            "Dial rotated by {} to {}, {} zeroes found...",
+            line, dial, zeroes
+        );
+    }
+    println!("Dial reads {}!", dial);
+    println!("{} zeroes found!", zeroes);
+}
--- src/main.rs
+++ src/main.rs
@@ -8,42 +8,46 @@ fn main() {
     let day = arg.next();
     let dayref = day.as_ref();
     match dayref.map(|i| i.as_str()) {
-        Some("1") => {
+        Some("1-1") => {
             println!("Running AOC 2025 Day 1");
-            day1::run();
+            day1::part1();
         }
-        Some("2") => {
-            println!("Running AOC 2025 Day 2");
+        Some("1-2") => {
+            println!("Running AOC 2025 Day 1 part 2");
+            day1::part2();
         }
-        Some("3") => {
-            println!("Running AOC 2025 Day 3");
+        Some("2-1") => {
+            println!("Running AOC 2025 Day 2 part 1");
         }
-        Some("4") => {
-            println!("Running AOC 2025 Day 4");
+        Some("3-1") => {
+            println!("Running AOC 2025 Day 3 part 1");
         }
-        Some("5") => {
-            println!("Running AOC 2025 Day 5");
+        Some("4-1") => {
+            println!("Running AOC 2025 Day 4 part 1");
         }
-        Some("6") => {
-            println!("Running AOC 2025 Day 6");
+        Some("5-1") => {
+            println!("Running AOC 2025 Day 5 part 1");
         }
-        Some("7") => {
-            println!("Running AOC 2025 Day 7");
+        Some("6-1") => {
+            println!("Running AOC 2025 Day 6 part 1");
         }
-        Some("8") => {
-            println!("Running AOC 2025 Day 8");
+        Some("7-1") => {
+            println!("Running AOC 2025 Day 7 part 1");
         }
-        Some("9") => {
-            println!("Running AOC 2025 Day 9");
+        Some("8-1") => {
+            println!("Running AOC 2025 Day 8 part 1");
         }
-        Some("10") => {
-            println!("Running AOC 2025 Day 10");
+        Some("9-1") => {
+            println!("Running AOC 2025 Day 9 part 1");
         }
-        Some("11") => {
-            println!("Running AOC 2025 Day 11");
+        Some("10-1") => {
+            println!("Running AOC 2025 Day 10 part 1");
         }
-        Some("12") => {
-            println!("Running AOC 2025 Day 12");
+        Some("11-1") => {
+            println!("Running AOC 2025 Day 11 part 1");
+        }
+        Some("12-1") => {
+            println!("Running AOC 2025 Day 12 part 1");
         }
         None | Some(_) => {
             println!("Invalid input, exiting!");
