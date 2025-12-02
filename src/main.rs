3734bf22a5d9393f434971d99cf5b8184cbf20b0
null-version-nova <ethcoyle@gmail.com>
2025-12-02 10:45:10 -0600

    added all functions to main. hopefully won't have to change it now?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::env::args;
 
 mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 
 fn main() {
     let mut arg = args();
-    arg.next();
-    let day = arg.next();
-    let dayref = day.as_ref();
-    match dayref.map(|i| i.as_str()) {
+    arg.next(); // We don't need the PWD...
+    match arg.next().as_ref().map(|i| i.as_str()) {
         Some("1-1") => {
             println!("Running AOC 2025 Day 1");
             day1::part1();
@@ -18,38 +27,93 @@ fn main() {
         }
         Some("2-1") => {
             println!("Running AOC 2025 Day 2 part 1");
+            day2::part1();
+        }
+        Some("2-2") => {
+            println!("Running AOC 2025 Day 2 part 2");
+            day2::part2();
         }
         Some("3-1") => {
             println!("Running AOC 2025 Day 3 part 1");
+            day3::part1();
+        }
+        Some("3-2") => {
+            println!("Running AOC 2025 Day 3 part 2");
+            day3::part2();
         }
         Some("4-1") => {
             println!("Running AOC 2025 Day 4 part 1");
+            day4::part1();
+        }
+        Some("4-2") => {
+            println!("Running AOC 2025 Day 4 part 2");
+            day4::part2();
         }
         Some("5-1") => {
             println!("Running AOC 2025 Day 5 part 1");
+            day5::part1();
+        }
+        Some("5-2") => {
+            println!("Running AOC 2025 Day 5 part 2");
+            day5::part2();
         }
         Some("6-1") => {
             println!("Running AOC 2025 Day 6 part 1");
+            day6::part1();
+        }
+        Some("6-2") => {
+            println!("Running AOC 2025 Day 6 part 2");
+            day6::part2();
         }
         Some("7-1") => {
             println!("Running AOC 2025 Day 7 part 1");
+            day7::part1();
+        }
+        Some("7-2") => {
+            println!("Running AOC 2025 Day 7 part 2");
+            day7::part2();
         }
         Some("8-1") => {
             println!("Running AOC 2025 Day 8 part 1");
+            day8::part1();
+        }
+        Some("8-2") => {
+            println!("Running AOC 2025 Day 8 part 2");
+            day8::part2();
         }
         Some("9-1") => {
             println!("Running AOC 2025 Day 9 part 1");
+            day9::part1();
+        }
+        Some("9-2") => {
+            println!("Running AOC 2025 Day 9 part 2");
+            day9::part2();
         }
         Some("10-1") => {
             println!("Running AOC 2025 Day 10 part 1");
+            day10::part1();
+        }
+        Some("10-2") => {
+            println!("Running AOC 2025 Day 10 part 2");
+            day10::part2();
         }
         Some("11-1") => {
             println!("Running AOC 2025 Day 11 part 1");
+            day11::part1();
+        }
+        Some("11-2") => {
+            println!("Running AOC 2025 Day 11 part 2");
+            day11::part2();
         }
         Some("12-1") => {
             println!("Running AOC 2025 Day 12 part 1");
+            day12::part1();
         }
-        None | Some(_) => {
+        Some("12-2") => {
+            println!("Running AOC 2025 Day 12 part 2");
+            day12::part2();
+        }
+        _ => {
             println!("Invalid input, exiting!");
         }
     }
